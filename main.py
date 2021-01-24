e0bc16ddc9bfe7d87af55d93138d94525ddaaaae
Jason Jenkins <jdj0891@yahoo.com>
2021-01-24 13:50:01 -0600

    Ready for Prod

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donor, Donation
 
 app = Flask(__name__)
 
@@ -11,11 +11,26 @@ app = Flask(__name__)
 def home():
     return redirect(url_for('all'))
 
-@app.route('/donations/')
+@app.route('/donations')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+
+@app.route('/donate', methods=['GET','POST'])
+def donate():
+    if request.method == 'POST':
+        donor = Donor.select().where(Donor.name == request.form['name'])
+
+        if not donor:
+            donor = Donor(name=request.form['name'])
+            donor.save()
+
+        Donation(donor=donor, value=request.form['amount']).save()
+        return redirect(url_for('home'))
+
+    return render_template('donate.jinja2')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
